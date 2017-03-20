65293c1b232479a812bc4d0776f9502db7767d93
HolySys <mcolombo87@gmail.com>
2017-03-20 01:07:04 -0300

    Initial Commit

---
+++ OrangeInstaller/OrangeInstaller/Installer.py
@@ -0,0 +1,3 @@
+class Installer(object):
+    """description of class"""
+    
---
+++ OrangeInstaller/OrangeInstaller/OrangeInstaller.py
@@ -0,0 +1,7 @@
+'''
+Project: OpenInstaller
+Author: Colombo Maximiliano
+Documentation: https://drive.google.com/drive/folders/0B4CKD2TRMlffOXlyUWVzSFE5ZnM
+Project Repository: https://github.com/mcolombo87/OrangeInstaller
+'''
+
